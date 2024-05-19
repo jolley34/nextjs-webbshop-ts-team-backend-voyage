64b53c16baa496ad630b25d0f1533b008e481b3b
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-05-19 16:50:32 +0200

    added handlers for sign-up and sign-in, unsure if right

--- app/actions/userActions.ts
+++ app/actions/userActions.ts
@@ -1,9 +1,15 @@
 "use server";
 
 import argon2 from "argon2";
+import { NextApiRequest, NextApiResponse } from "next";
 import { revalidatePath } from "next/cache";
 import { db } from "../../prisma/db";
-import { UserCreate, UserCreateSchema } from "../validation/validation";
+import {
+  UserCreate,
+  UserCreateSchema,
+  UserSignIn,
+  UserSignInSchema,
+} from "../validation/validation";
 
 export async function signUpUser(incomingData: UserCreate) {
   const userData = UserCreateSchema.parse(incomingData);
@@ -19,3 +25,48 @@ export async function signUpUser(incomingData: UserCreate) {
   revalidatePath("/");
   return user;
 }
+
+export async function signInUser(incomingData: UserSignIn) {
+  const signInData = UserSignInSchema.parse(incomingData);
+
+  const user = await db.user.findUnique({
+    where: {
+      id: 5, // Behövs?? Temporärt hårkodad just nu
+      username: signInData.username,
+    },
+  });
+
+  if (user && (await argon2.verify(user.password, signInData.password))) {
+    revalidatePath("/");
+    return user;
+  } else {
+    throw new Error("Invalid username or password");
+  }
+}
+
+export async function signUpHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+      try {
+        const user = await signUpUser(req.body);
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(400).json("Invalid username or password");
+      }
+    } else {
+      res.status(405).json("Method not allowed");
+    }
+  }
+  
+  export async function signInHandler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+      try {
+        const user = await signInUser(req.body);
+        res.status(200).json(user);
+      } catch (error) {
+        res.status(400).json("Invalid username or password");
+      }
+    } else {
+      res.status(405).json("Method not allowed");
+    }
+  }
+  
