3f911471991a65632cf776ccf539c4064cc981f5
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-05-19 09:58:32 +0200

    Created sign-in page, without functions for now only ui

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import Link from "next/link";
+import theme from "../themes/themes";
+
+export default function SignIn() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid
+        container
+        justifyContent="center"
+        sx={{
+          bgcolor: "#f6f5f3",
+          padding: "1rem",
+          minHeight: "50vh",
+          paddingTop: { xs: "97px", sm: "104px", md: "104px" },
+        }}
+      >
+        <Box
+          sx={{
+            margin: "8rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: "white",
+            padding: "2rem",
+            boxShadow: "0 0.125rem 0.25rem rgba(0, 0, 0, 0.075)",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2" style={{ color: "black" }}>
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Grid>
+    </ThemeProvider>
+  );
+}
