38db254c7168b808fd912e67b681d43201d7aa9c
jolley34 <erlandssonjoel@gmail.com>
2024-06-03 16:03:45 +0200

    okokok

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,9 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-/* neonConfig.webSocketConstructor = WebSocket;
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool); */
 export const db = new PrismaClient().$extends(withAccelerate());
