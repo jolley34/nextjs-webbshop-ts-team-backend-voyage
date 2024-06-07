4910bcf52543ab4e4350fb4d90cccb24a513b421
jolley34 <erlandssonjoel@gmail.com>
2024-06-07 11:10:30 +0200

    quck fix

--- app/my-page/page.tsx
+++ app/my-page/page.tsx
@@ -1,40 +1,45 @@
+import { auth } from "@/auth";
 import { Box, Card, Typography } from "@mui/material";
 import Link from "next/link";
 
-export default function indexPage() {
+export default async function indexPage() {
+  const session = await auth();
+
   return (
     <>
-      <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-        <Link style={{ textDecoration: "none" }} href="/my-page/track-order">
-          <Card
-            sx={{
-              padding: "2rem",
-              cursor: "pointer",
-              transition: "background 0.5s ease",
-              "&:hover": {
-                background: "rgba(241, 241, 241, 0.806)",
-              },
-            }}
-          >
-            <Typography>Track your order</Typography>
-          </Card>
-        </Link>
+      {session?.user && (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+          <Link style={{ textDecoration: "none" }} href="/my-page/track-order">
+            <Card
+              sx={{
+                padding: "2rem",
+                cursor: "pointer",
+                transition: "background 0.5s ease",
+                "&:hover": {
+                  background: "rgba(241, 241, 241, 0.806)",
+                },
+              }}
+            >
+              <Typography>Track your order</Typography>
+            </Card>
+          </Link>
 
-        <Link style={{ textDecoration: "none" }} href="/my-page/order">
-          <Card
-            sx={{
-              padding: "2rem",
-              cursor: "pointer",
-              transition: "background 0.5s ease",
-              "&:hover": {
-                background: "rgba(241, 241, 241, 0.806)",
-              },
-            }}
-          >
-            <Typography>My orders</Typography>
-          </Card>
-        </Link>
-      </Box>
+          <Link style={{ textDecoration: "none" }} href="/my-page/order">
+            <Card
+              sx={{
+                padding: "2rem",
+                cursor: "pointer",
+                transition: "background 0.5s ease",
+                "&:hover": {
+                  background: "rgba(241, 241, 241, 0.806)",
+                },
+              }}
+            >
+              <Typography>My orders</Typography>
+            </Card>
+          </Link>
+        </Box>
+      )}
     </>
   );
 }
